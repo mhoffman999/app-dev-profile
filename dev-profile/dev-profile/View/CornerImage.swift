3cedb670bcca363ca04735eb168e35258e433f8c
Michael Hoffman <mhoffman999@gmail.com>
2018-12-15 15:05:49 -0500

    Laid Storyboard elements for iPhone only layout.

---
+++ dev-profile/dev-profile/View/CornerImage.swift
@@ -0,0 +1,26 @@
+//
+//  CornerImage.swift
+//  dev-profile
+//
+//  Created by Michael Hoffman on 12/15/18.
+//  Copyright © 2018 Here We Go. All rights reserved.
+//
+
+import UIKit
+
+class CornerImage: UIImageView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        layer.cornerRadius = 3.0
+    }
+
+}
